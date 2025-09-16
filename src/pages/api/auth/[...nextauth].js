3a5e79293e3d8835a529b440140f18d1805bb4b7
Devarh sutariya <devarshsutariya298@gmail.com>
2025-09-16 10:51:52 +0530

    env updated

--- src/pages/api/auth/[...nextauth].js
+++ src/pages/api/auth/[...nextauth].js
@@ -5,8 +5,8 @@ import GithubProvider from "next-auth/providers/github";
 export const authOptions = {
     providers: [
         GithubProvider({
-            clientId:'Iv23lizHw1uVRJRwH0AF',
-            clientSecret:'4ceefddc8d17a776a4aff8084935ba95b2bffea3',
+            clientId: process.env.clientId,
+            clientSecret: process.env.clientSecret,
         }),
     ],
 };
